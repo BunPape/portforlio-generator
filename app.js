4c3f1706bb12c602b9aaaffcc0dbffed7e7b2aa0
Bun Pape <benjaminhpage@me.com>
2022-01-27 13:50:31 -0500

    wrtie file and src folder added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const profileDataArr = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
-    return `
-    Name: ${userName}
-    Github: ${githubName}
-    `;
-};
-console.log(generatePage('Jane', 'Janehub'));
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the outpu!')
+});
 
 // const printprofileData = (profileDataArr) => {
 //     for (let i = 0; i < profileDataArr.length; i++) {
