eae6dc3ff3e41081c410f5887e3efe5fb8f179be
Bun Pape <benjaminhpage@me.com>
2022-01-28 15:23:45 -0500

    updated template with .map() and .filter() to generate different style for featured or not proects. Error on line 21 at .filter() cannot figure out wtf is wrong

--- app.js
+++ app.js
@@ -129,30 +129,12 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers));
-
-
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
-
-// const printprofileData = (profileDataArr) => {
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('============');
-
-//     // is the same as this...
-//     profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-
-// printprofileData(profileDataArr);
+    .then(portfolioData => {
+      const pageHTML = generatePage(portfolioData);
+  
+      fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+  
+        console.log('Page created! Check out index.html in this directory to see it!');
+      });
+    });
